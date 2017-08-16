7fd6129deab69cf6689423c88db569d425e0f344
CainaOliboni <cainaoliboni4@gmail.com>
2017-08-16 16:34:39 -0300

    Novas Implementações websocket

--- src/main/webapp/paginaInicialTeste/paginaInicialTeste.js
+++ src/main/webapp/paginaInicialTeste/paginaInicialTeste.js
@@ -1,36 +1,60 @@
 var ws;
 var output;
 
-function init()
-{
-  output = document.getElementById("output");
-  testWebSocket();
-}
-
-if (window.WebSocket) {
-	ws = new WebSocket("ws://echo.websocket.org");
-	ws.onopen = function(evt) { onOpen(evt) };
-	ws.onclose = function(evt) { onClose(evt) };
-	ws.onmessage = function(evt) { onMessage(evt) };
-	ws.onerror = function(evt) { onError(evt) };
+function abrirConexao() {
 	
+	if (window.WebSocket) {
+		
+		output = document.getElementById("output");
+		
+		ws = new WebSocket("ws://echo.websocket.org");
+		ws.onopen = function(evt) { onOpen(evt) };
+		ws.onclose = function(evt) { onClose(evt) };
+		ws.onmessage = function(evt) { onMessage(evt) };
+		ws.onerror = function(evt) { onError(evt) };
+	    
+	    console.log("WebSocket supported in your browser");
+		
+	} else {
+		console.log("WebSocket not supported in your browser");
+	}
 	
-	
-	ws.onmessage = function (e) {
-		// e.data contains received string.
-		console.log("echo from server : " + e.data);
-	};
-	
-	ws.onclose = function () { console.log("onclose"); };
-	ws.onerror = function () { console.log("onerror"); };
-} else {
-	console.log("WebSocket not supported in your browser");
-}
-
-if (ws.readyState === 1) ws.send("OI SERVER");
-
-function onopen = function () {
 	console.log("onopen");
+	
 };
 
+function onOpen(){
+	
+	console.log("ABRIU")
+};
 
+function onMessage (evt) {
+	
+	writeToScreen("<span> MENSAGEM RETORNO: " + evt.data.value + "</span>");
+	
+	console.log(evt);
+
+};
+
+function enviarMensagem() {
+	
+	var msg = document.getElementById("msgWebSocket");
+	ws.send(msg);
+	
+};
+
+function onClose() { 
+	
+	ws.close();
+	
+	console.log("onclose"); 
+	
+};
+function onError() { console.log("onerror"); };
+
+function writeToScreen(message){
+	
+  var pre = document.createElement("p");
+  pre.innerHTML = message;
+  output.appendChild(pre);
+};
